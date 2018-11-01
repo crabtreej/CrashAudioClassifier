d80373b4f7b87ba2a25e0c7a03dba65e374ef107
Jacob Crabtree <crabtree.234@buckeyemail.osu.edu>
2018-11-01 13:16:35 -0400

    Added code to find the best C parameter for a LinearSVC. Uses grid cross validation, and averages the best C value. Can also take user input for C-values to test.

--- Code/main.py
+++ Code/main.py
@@ -1,14 +1,11 @@
-import sys
-import parseXMLData
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 import pickle
 from sklearn.cluster import KMeans
-from sklearn.svm import SVC
-from scipy import signal
-import matplotlib.pyplot as plt
+from sklearn.svm import SVC, LinearSVC
 import numpy as np
-#import sounddevice as sd
-import time
 from pyAudioAnalysis import audioFeatureExtraction as fe
+from sklearn.metrics import classification_report
+from scipy.stats import norm
 
 def getHistogramsAndMembershipFromKMeans(kmeansCLF, classToClipsMFCCsMap):
     histograms = []
@@ -23,6 +20,49 @@ def getHistogramsAndMembershipFromKMeans(kmeansCLF, classToClipsMFCCsMap):
             classMembership.append(classID)
 
     return (histograms, classMembership)
+
+def findBestParamForLinearSVClassifier(trainingHistograms, trainingLabels, validationHistograms, validationLabels):
+
+    c_value = '' 
+    #can change this to take input from user if you want to test values yourself by setting c_value = the line below.
+    #input('Input a list of C-values (one by one) to test, or blank to see a random sampling from around what I\'ve found to be the best values.\n')
+
+    if c_value == '':
+        #i think 0.01 is best, so sample around 0.01 randomly with some values
+        c_values = [0.01, 0.011, 0.012, 0.0115, 0.0125, 0.018, 0.0099, 0.009, 0.0095, 0.0085, 0.0080, 0.0075, 0.007, 0.0065, 0.006, 0.005, 0.0055, 0.0051, 0.0049, 0.0045, 0.004, 0.0119, 0.0093] 
+    else:
+        c_values = [float(c_value)]
+        while(c_value != ''):
+            c_value = input()
+            if c_value != '':
+                c_values.append(float(c_value))
+    clf = GridSearchCV(LinearSVC(max_iter=10000), {'C': c_values}, cv=3, scoring='accuracy')
+
+    clf.fit(trainingHistograms, trainingLabels)
+
+    #print("Best parameters set found on development set:")
+    #print()
+    #print(clf.best_params_)
+    #print()
+    #print("Grid scores on development set:")
+    #print()
+    #means = clf.cv_results_['mean_test_score']
+    #stds = clf.cv_results_['std_test_score']
+    #for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+    #    print("%0.3f (+/-%0.03f) for %r"
+    #          % (mean, std * 2, params))
+    #print()
+
+    #print("Detailed classification report:")
+    #print()
+    #print("The model is trained on the full development set.")
+    #print("The scores are computed on the full evaluation set.")
+
+    y_true, y_pred = validationLabels, clf.predict(validationHistograms)
+    #print(classification_report(y_true, y_pred))
+    #print()
+
+    return clf.best_params_['C']
 
 if __name__ == '__main__':
 
@@ -58,22 +98,16 @@ if __name__ == '__main__':
 
     validationHistograms, validationLabels = getHistogramsAndMembershipFromKMeans(kmeans, classToClipsAsFramesOfMFCCsMapC)
 
-    print(trainingHistograms)
-    print(trainingLabels)
-
     #now we have training data for an svm
-    mySVM = SVC(gamma='auto')
-    mySVM.fit(trainingHistograms, trainingLabels)
-    print(mySVM.score(validationHistograms, validationLabels))
+   
+    #determined dual=True is twice as fast
 
-
-
-
-
-
-
-
-
-
-
-
+    #If you run this code, you'll find that the best parameter for a LinearSVC for this data is
+    #0.00999999... = 0.01. I tried values much smaller that that, and up to 10^7 for the C parameter,
+    #and 0.01 is the best. This just averages the values found over a large number of iterations in an
+    #attempt to remove any variance from it.
+    #bestC = 0
+    #for i in range(0, 1000): 
+    #    bestC = bestC + findBestParamForLinearSVClassifier(trainingHistograms, trainingLabels, validationHistograms, validationLabels)
+    #bestC = bestC / 1000.0
+    #print(f'Best C from averaging was: {bestC}')
